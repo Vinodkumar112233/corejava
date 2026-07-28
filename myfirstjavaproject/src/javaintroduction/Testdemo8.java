03577e02cfb214a573375428bc57832e72dee8f4
Vinodkumar112233 <vinodkumarbarnana@gmail.com>
2026-07-28 09:46:36 +0530

    commit

---
+++ myfirstjavaproject/src/javaintroduction/Testdemo8.java
@@ -0,0 +1,19 @@
+package javaintroduction;
+
+public class Testdemo8 {
+	int total_money = 100;
+	int cost_of_pencil = 7;
+	int total_pencils_bought = 14;
+	int remaining_money = 100%7;
+
+	public static void main(String[] args) {
+		Testdemo8 v = new Testdemo8();
+		System.out.println("Total Money: "+v.total_money);
+		System.out.println("Cost Of Pencil: "+v.cost_of_pencil);
+		System.out.println("Total Pencils Bought: "+v.total_pencils_bought);
+		System.out.println("Remaining Money: "+v.remaining_money);
+		
+
+	}
+
+}
